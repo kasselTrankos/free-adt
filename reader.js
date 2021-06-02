e23e15b3a892d053f56646c7484c61c7f9e9da7e
kasselTrankos <alvaro.touzon@gmail.com>
2021-06-02 22:04:08 +0200

    feat: logger and star understand reader adt

--- reader.js
+++ reader.js
@@ -1,57 +1,47 @@
 // reader
-const daggy = require('daggy')
-const compose = (f, g) => x => f(g(x))
 
-//  Reader e a
-const Reader = daggy.tagged('Reader', ['f'])
+const { Monad } = require('./utils')
+const { toUpper, converge, compose, concat, flip } = require('ramda')
+const { Reader } = require('./adt/reader')
+const process = require('process');
+const {ask} = Reader
 
-Reader.prototype.map = function(f) {
-    return Reader(compose(this.f, f))
-}
+// greet :: String -> String -> Reader String String
+const greet = greeting => str => `${greeting} ${str}` 
 
-Reader.prototype.runWith = function(x) {
-    return this.f(x)
-}
-
-// (a -> m b) -> m b
-Reader.prototype.chain = function(that) {
-    console.log(this.f.toString(), that)
-    // console.log(this.f.toString(), that.f.toString())
-    return this.f(x => console.log(x, '11111') ||  that(x))
-    // return compose(this.f, that.f)
-}
-
-// Reader.ask :: (e -> b) -> Reader e b
-Reader.ask = function(f) {
-    const g = f ? f: x => x
-    return Reader(g)
-}
-const add =
-  x => y => x + y
-
-const a = Reader.ask(add(10)).runWith(56)
-
-const greet = greeting =>
-  Reader(name => `${greeting}, ${name}`)
-
+// addFarewell :: String -> Reader String String
 const addFarewell = farewell => str =>
-  Reader.ask(env => `${str}${farewell} ${env}`)
-const concat = x => y => `${x}${y}`
-const flow =
-  greet('Hola')
-  .chain(m => console.log(m) || Reader(x => m + 'hola'))
-//   .map(concat('...'))
-//   .chain(x => console.log(x, '1239') || Reader(g => x + g ))
-const b =flow
-    .runWith('Jenny')
-
-console.log(b)
-// const f = x => x * 3
-// const g = x => x + 2 
-
-// console.log(compose(f, g)(2))
-
-// const greeet = Reader(name => `${greeting}, ${name}`)
+  ask(env => `${str}${farewell} ${env}`)
 
 
+const a = Reader(greet('hola'))
+  .map(toUpper)
+  .chain(addFarewell(' alo que '))
+  .runWith('Maria')
 
+const getFullYear = d => d.getFullYear()
+const getMonth = d => d.getMonth()
+const getDay = d => d.getDate()
+const addZeroToDate = x => x < 10 ? `0${x}` : x
+const toNumber = x => Number(x)
+const formatDate = converge((d, m, y)=> `${d}-${m}-${y}`, [
+  compose(addZeroToDate, toNumber, getDay), 
+  compose(addZeroToDate ,toNumber, getMonth), 
+  getFullYear
+])
+
+const addDate = x => `(${new Date()}) ${x}`
+const wrapWith = l => r => str => `${l}${str}${r}`
+
+
+const logger = Reader.of(new Date())
+  .map(formatDate)
+  .map(wrapWith('(')(')'))
+  .map(flip(concat)(': '))
+  
+  // .chain(x => Reader(concat(process.id)))
+  .chain(x => Reader(concat(x)))
+  .map(concat(' <> '))
+  .map(concat(`[${process.pid}]`))
+  .runWith('jo que error')
+console.log(logger)
